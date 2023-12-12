8e95d4d5c6c4229b3eae76cdc498ad4be445a1fd
Ango2000 <ango2@babson.edu>
2023-12-11 19:01:02 -0500

    update

--- app.py
+++ app.py
@@ -1,12 +1,55 @@
-from flask import Flask, render_template
-
-from champion_data import champion_list
+from flask import Flask, jsonify, render_template
+import requests
+from champion_data import latest_champ_data, full_champ_data
 
 app = Flask(__name__)
 
+
+
+# @app.route('/champion-champion-list')
+# def get_champion_list():
+#     url_for_champ_list = "https://ddragon.leagueoflegends.com/api/versions.json"
+#     champ_list_response = requests.get(url_for_champ_list)
+#     champion_list = []
+#     if champ_list_response.status_code == 200:
+#         versions = champ_list_response.json()
+#         latest_version = versions[]
+#         latest_champion_data_url = requests.get(latest_champion_data_url)
+#         if latest_champion_data_response.status_code == 200:
+#             aggregate_champ_data = 
+#     champion_data = champion_list()
+#     return jsonify(champion_data)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+# def fetch_champion_data_list():
+#     full_champion_data_list = champion_data_list
+
+#     return full_champion_data_list
+
+# def latest_champ_data():
+
+
+# def full_champ_data(champion_list):
+
 @app.route('/')
-def index ():
-    return render_template('index.html', champion_list=champion_list)
+def index():
+    champion_list = latest_champ_data()
+    champion_data_list = full_champ_data(champion_list)
+    return render_template('base_champ_select.html', champion_data_list=champion_data_list)
+
+@app.route('/champion/<champion_name>')
+def get_champion(champion_name):
+    champion_list = latest_champ_data()
+    champion_data_list = full_champ_data(champion_list)
+    champion_info = next ((champion for champion in champion_data_list if champion['name'] == champion_name), None)
+    if champion_info:
+        return jsonify(champion_info)
+    else:
+    # champion_list = latest_champ_data()
+    # champion_data_list = full_champ_data(champion_list)
+    # return jsonify({"champion_data_list": champion_data_list})
+        return jsonify({"error": "Champion not found"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
--- champion_data.py
+++ champion_data.py
@@ -6,84 +6,96 @@ import requests
 # with open ('keys.json', 'r') as config_file:
 #     config = json.load(config_file)
 
-url_for_champ_list = "https://ddragon.leagueoflegends.com/api/versions.json"
-# "https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/champion.json"
 
-champ_list_response = requests.get(url_for_champ_list)
+def latest_champ_data():
+    url_for_champ_list = "https://ddragon.leagueoflegends.com/api/versions.json"
+    # "https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/champion.json"
 
-champion_list = []
+    champ_list_response = requests.get(url_for_champ_list)
 
-if champ_list_response.status_code == 200:
-    """
-    finds the latest version/patch of the aggregate champion data
-    """
-    # champion_list = []
-    # aggregate_champ_list = champ_list_response.json()
-    versions = champ_list_response.json()
-    latest_version = versions[0]
-    latest_champion_data_url = f"https://ddragon.leagueoflegends.com/cdn/{latest_version}/data/en_US/champion.json"
-    latest_champion_data_response = requests.get(latest_champion_data_url)
-    if latest_champion_data_response.status_code == 200:
+    champion_list = []
+    if champ_list_response.status_code == 200:
         """
-        takes the json file that contains all basic champion data in the game and stores it 
+        finds the latest version/patch of the aggregate champion data
         """
-        aggregate_champ_data = latest_champion_data_response.json()
-        champ_data_temp = aggregate_champ_data["data"]
-        print(champion_list)
-        for champ in champ_data_temp.values():
+        # champion_list = []
+        # aggregate_champ_list = champ_list_response.json()
+        versions = champ_list_response.json()
+        latest_version = versions[0]
+        latest_champion_data_url = f"https://ddragon.leagueoflegends.com/cdn/{latest_version}/data/en_US/champion.json"
+        latest_champion_data_response = requests.get(latest_champion_data_url)
+        if latest_champion_data_response.status_code == 200:
             """
-            it iterates through the stored data to pull the id which is the champ's name
-            that is used when accessing champion specific json files
+            takes the json file that contains all basic champion data in the game and stores it 
             """
-            champion_list.append(champ["id"])
-else:
-    print("Unable to pull champion data")
-
-base_url = "https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/champion/"
-
-champion_data_list = []
-
-for champ in champion_list:
-    """
-    takes the base url and inserts the champion id from champion_list 
-    """
-    champion_url = f"{base_url}{champ}.json"
-    response = requests.get(champion_url)
-
-    if response.status_code == 200:
-        """
-        stores individual champ json data in champion_data
-        """
-        champion_data = response.json()
-        if champ in champion_data["data"]:
-            """
-            iterates through each individual champion json file and pulls specific data
-            and appends that data into champion_data_list
-            """
-            temp_champ_data = champion_data["data"][champ]
-            champion_info = {
-                "name": temp_champ_data["name"],
-                "allytips": temp_champ_data.get("allytips", []),
-                "enemytips": temp_champ_data.get("enemytips", []),
-                "tags": temp_champ_data.get("tags", []),
-                "image_full": temp_champ_data["image"]["full"]
-            }
-            champion_data_list.append(champion_info)
-        else:
-            print(f"Unable to pull {champ} data")
-
+            aggregate_champ_data = latest_champion_data_response.json()
+            champ_data_temp = aggregate_champ_data["data"]
+            print(champion_list)
+            for champ in champ_data_temp.values():
+                """
+                it iterates through the stored data to pull the id which is the champ's name
+                that is used when accessing champion specific json files
+                """
+                champion_list.append(champ["id"])
     else:
-        print(f"Unable to load {champ} data ")
+        print("Unable to pull champion data")
+    return champion_list
+# latest_champ_data()
 
-for champion_info in champion_data_list:
-    """
-    prints champion data in a formatted way for each champion
-    """
-    print("Champion Name:", champion_info["name"])
-    print("Ally Tips:", champion_info["allytips"])
-    print("Enemy Tips:", champion_info["enemytips"])
-    print("Tags:", champion_info["tags"])
-    print("Image:", champion_info["image_full"])
-    # will need to add code where if a field is blank, it should print a statement
-    # saying that there is no data on that field
-print (champion_list)
+
+
+def full_champ_data(champion_list):
+    base_url = "https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/champion/"
+
+    champion_data_list = []
+    for champ in champion_list:
+        """
+        takes the base url and inserts the champion id from champion_list 
+        """
+        champion_url = f"{base_url}{champ}.json"
+        response = requests.get(champion_url)
+
+        if response.status_code == 200:
+            """
+            stores individual champ json data in champion_data
+            """
+            champion_data = response.json()
+            if champ in champion_data["data"]:
+                """
+                iterates through each individual champion json file and pulls specific data
+                and appends that data into champion_data_list
+                """
+                temp_champ_data = champion_data["data"][champ]
+                champion_info = {
+                    "name": temp_champ_data["name"],
+                    "allytips": temp_champ_data.get("allytips", []),
+                    "enemytips": temp_champ_data.get("enemytips", []),
+                    "tags": temp_champ_data.get("tags", []),
+                    "image_full": temp_champ_data["image"]["full"]
+                }
+                champion_data_list.append(champion_info)
+            else:
+                print(f"Unable to pull {champ} data")
+
+        else:
+            print(f"Unable to load {champ} data ")
+    return champion_data_list
+# full_champ_data()
+
+def show_champ_data(champion_data_list):
+    for champion_info in champion_data_list:
+        """
+        prints champion data in a formatted way for each champion
+        """
+        print("Champion Name:", champion_info["name"])
+        print("Ally Tips:", champion_info["allytips"])
+        print("Enemy Tips:", champion_info["enemytips"])
+        print("Tags:", champion_info["tags"])
+        print("Image:", champion_info["image_full"])
+        # will need to add code where if a field is blank, it should print a statement
+        # saying that there is no data on that field
+# print (champion_list)
+
+champion_list = latest_champ_data()
+champion_data_list = full_champ_data(champion_list)
+show_champ_data(champion_data_list)
